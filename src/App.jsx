93faeda11c664859121cddbf355a1e8ad68fddc3
hendryprasetyo <hendryprras@gmail.com>
2023-11-03 10:42:27 +0700

    init folderstructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { callAPI } from './domain/api'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    callAPI({ baseUrl: 'ytest' })
+  }, [])
 
   return (
     <>
@@ -18,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
