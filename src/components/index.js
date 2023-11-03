84fcf49d5f45feb983f14a56fff0934b6690f3e6
adamarfansyah <adam.arfansyah@phincon.com>
2023-11-03 16:37:13 +0700

    bug fixed navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as ProductCard } from './ProductCard'
-export { default as Navbar } from './Navbar'
-export { default as Layout } from './Layouts'
-export { default as CartItem } from './CartItem'
+export { default as ProductCard } from "./ProductCard";
+export { default as Navbar } from "./Navbar";
+export { default as Layout } from "./Layouts";
+export { default as CartItem } from "./CartItem";
+export { default as BannerHome } from "./BannerHome";
