b505db5e3622ca7654a9cdf25a3c151e59e4ef78
hendryprasetyo <hendryprras@gmail.com>
2023-11-03 14:31:03 +0700

    create functionality for connect to json-server - add to cart - remove to cart

--- src/hook/useCart.jsx
+++ src/hook/useCart.jsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react'
 import { callAPI } from '../domain/api'
 import { ProductContext } from '../context/ProductContext'
-
+import toast from 'react-hot-toast'
 const useCart = () => {
-  const { setCart, setLoading } = useContext(ProductContext)
+  const { setCart, setLoading, setErrorMsg } = useContext(ProductContext)
   const baseUrl = import.meta.env.VITE_BASE_URL_JSON_SERVER
 
   const getCart = async () => {
@@ -19,9 +19,54 @@ const useCart = () => {
       setLoading(false)
     }
   }
-
+  const addToCart = async data => {
+    setLoading(true)
+    try {
+      const { productId, quantity, ...rest } = data
+      const findProduct = await callAPI({
+        endpoint: `/cart?productId=${productId}`,
+        method: 'GET',
+        baseUrl,
+      })
+      if (findProduct) {
+        const newQty = quantity + findProduct?.quantity
+        const newData = { quantity: newQty, rest }
+        await callAPI({
+          endpoint: `/cart/${findProduct?.id}`,
+          baseUrl,
+          method: 'PATH',
+          newData,
+        })
+      } else {
+        await callAPI({
+          endpoint: '/cart',
+          baseUrl,
+          method: 'POST',
+          data,
+        })
+      }
+      toast.success('Added to cart')
+    } catch (error) {
+      setErrorMsg(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const removeProductCart = async cartId => {
+    setLoading(true)
+    try {
+      await callAPI({ endpoint: `/cart/${cartId}`, method: 'DELETE', baseUrl })
+      toast.success('Remove product from cart')
+    } catch (error) {
+      setErrorMsg(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
   return {
     getCart,
+    addToCart,
+    removeProductCart,
   }
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { ProductProvider } from './context/ProductContext.jsx';
-import './styles/globals.scss';
-import { Layout } from './components/';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { ProductProvider } from './context/ProductContext.jsx'
+import './styles/globals.scss'
+import { Layout } from './components/'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ProductProvider>
       <Layout>
+        <Toaster />
         <App />
       </Layout>
     </ProductProvider>
   </React.StrictMode>
-);
+)
