8c67f66a4fc4fe884291b6cba45965d96c745f74
hendryprasetyo <hendryprras@gmail.com>
2023-11-03 16:04:36 +0700

    feat(home):list product and added banner

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Card } from '@mui/material'
+import styles from './ProductCard.module.scss'
+import { AddShoppingCart, Star } from '@mui/icons-material'
 
-const ProductCard = () => {
-  return <div>ProductCard</div>
+const ProductCard = ({ data, detailFunc, addToCart }) => {
+  const priceFormated = data?.price?.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+  return (
+    <Card className={styles.card}>
+      <div onClick={detailFunc}>
+        <img
+          src={data?.image}
+          className={styles.card__image}
+          alt="product-image"
+        />
+      </div>
+      <div className={styles.card__content}>
+        <h1 className={styles.card__name}>{data?.title}</h1>
+        <p className={styles.card__category}>{data?.category}</p>
+        <div className={styles.card__rating}>
+          <Star className={styles.card__icon__star} />{' '}
+          <span>{data?.rating?.rate}</span>{' '}
+          <span className={styles.card__count}>({data?.rating?.count})</span>
+        </div>
+      </div>
+      <div className={styles.card__footer}>
+        <p className={styles.card__price}>{priceFormated}</p>
+        <div className={styles.card__btn} onClick={addToCart}>
+          <AddShoppingCart />
+        </div>
+      </div>
+    </Card>
+  )
 }
 
 export default ProductCard
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export { default as ProductCard } from './ProductCard';
-export { default as Navbar } from './Navbar';
-export { default as Layout } from './Layouts';
+export { default as ProductCard } from './ProductCard'
+export { default as Navbar } from './Navbar'
+export { default as Layout } from './Layouts'
+export { default as BannerHome } from './BannerHome'
